3d4872c457282158cf8bf7ddd93b7b86a0524db8
hengyi_glb <nb18946973@gmail.com>
2021-08-17 09:43:57 +0800

    8-17

--- data/华文logo与文字/datahandle.py
+++ data/华文logo与文字/datahandle.py
@@ -16,18 +16,23 @@ if __name__ == '__main__':
     #     log('./info.txt', f"下标为：{index}，名字为：{name} 英文名字为: logo地址: \n")
 
     # 处理数据
-    # info = get_multiple_num('./info.txt', 1)[:100]
-    # info_list = []
-    # for index, each in enumerate(info):
-    #     each = each.split(' ')
-    #     name = each[2] if each[2] != '1' else ''
-    #     url = each[4] if each[4] != '1' else ''
-    #     info_list.append([name, url])
+    info = get_multiple_num('./info.txt', 1)[100:200]
+    info_list = []
+    for index, each in enumerate(info):
+        index_new = index + 100
+        each_new = each.split(' ')
+        # if len(each_new) != 5:
+        #     print(index_new)
+        name = each_new[2] if each_new[2] != '1' else ''
+        url = each_new[4] if each_new[4] != '1' else ''
+        info_list.append([name, url])
+
+    print(info_list)
 
     # 链接数据库
     # foreign_name = []
     # sql_name = 'select `组织外文名称` from `华文教育组织信息表`'
-    # name = list(db('219.130.114.4', 'user12', 'user12*#21USER', 'user12_db', 9210).get_all(sql_name))[:100]
+    # name = list(db('219.130.114.4', 'user12', 'user12*#21USER', 'user12_db', 9210).get_all(sql_name))[100:200]
     # for each in name:
     #     foreign_name.append(each[0])
     # foreign_name = enumerate(foreign_name)
@@ -36,7 +41,7 @@ if __name__ == '__main__':
     # for index, each in foreign_name:
     #     if each is None:
     #         new = info_list[index][0]
-    #         id_index = int(index) + 1
+    #         id_index = int(index) + 101
     #         sql = f"UPDATE `华文教育组织信息表` SET `组织外文名称` = '{new}' WHERE `序号` = {id_index}"
     #         db('219.130.114.4', 'user12', 'user12*#21USER', 'user12_db', 9210).operation(sql, '插入外文名字')
     # print('mission over')
@@ -44,21 +49,21 @@ if __name__ == '__main__':
     # 将链接下载为图片
     # for index, each in tqdm(enumerate(info_list)):
     #     if each[1] != '':
-    #         name = str(index + 1)
+    #         name = str(index + 101)
     #         urllib_download(each[1], name)
     # print('mission over')
 
     # 链接数据库
     foreign_logo = []
     sql_name = 'select `组织logo` from `华文教育组织信息表`'
-    name = list(db('219.130.114.4', 'user12', 'user12*#21USER', 'user12_db', 9210).get_all(sql_name))[:100]
+    name = list(db('219.130.114.4', 'user12', 'user12*#21USER', 'user12_db', 9210).get_all(sql_name))[100:200]
     for each in name:
         foreign_logo.append(each[0])
     foreign_name = enumerate(foreign_logo)
 
-    # 开始准备插入
+    # # 开始准备插入
     for index, each in foreign_name:
-        id_index = int(index) + 1
+        id_index = int(index) + 101
         if each is None:
             try:
                 with open(f"./img/{id_index}.jpg", 'rb') as f:
